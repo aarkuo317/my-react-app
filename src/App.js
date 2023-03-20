d032e06c6050ffffdb8565be9524708172800e76
aarkuo317 <c66578812@gmail.com>
2023-03-20 15:40:34 +0800

    try it again.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 
 function App() {
   return (
-<img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg" />
+    <div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          haha
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+    </div>
+    <div><img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg" /></div></div>
   );
 }
 
